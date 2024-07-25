62eead72222eb70d461eacf4565e5acdd1e1e9c1
xciphand <3070073429@qq.com>
2024-07-25 22:43:56 +0800

    :star2: 修复路径问题

--- decrypt.py
+++ decrypt.py
@@ -24,24 +24,48 @@ def getKeys(uid, path):
 
 
 def removeHead(keys, path):
-    newPath = "./decrypt_" + str(int(time.time()))
-    os.mkdir(newPath)
+    newPath = os.path.join(".", "decrypt_" + str(int(time.time())))
+    try:
+        os.mkdir(newPath)
+    except OSError as e:
+        print(f"创建目录 {newPath} 失败")
+        return None
     for db, _ in keys.items():
-        with open(path + "/" + db, "rb") as f:
-            f.seek(1024)
-            with open(newPath + "/en_" + db, "wb") as newf:
-                newf.write(f.read())
+        source_file = os.path.join(path, db)
+        dest_file = os.path.join(newPath, "en_" + db)
+        try:
+            with open(source_file, "rb") as f:
+                f.seek(1024)
+                with open(dest_file, "wb") as newf:
+                    newf.write(f.read())
+        except OSError as e:
+            print(f"处理文件 {db} 失败")
+            continue
     return newPath
 
 
 def decryptDB(keys, path):
     count = 0
+
+    def remove_file(file_path):
+        try:
+            os.remove(file_path)
+            return True
+        except:
+            return False
+
     for db, key in keys.items():
-        encrypted_db = path + "/en_" + db
-        unencrypted_db = path + "/" + db
+        encrypted_db = os.path.join(path, "en_" + db)
+        unencrypted_db = os.path.join(path, db)
+        if db.startswith("gpro_v1-6_u"):
+            print(f"暂不支持解密 {db}")
+            continue
+        else:
+            print(f"正在解密 {db}")
         if not os.path.exists(unencrypted_db):
             conn = sqlite.connect(unencrypted_db)
             conn.close()
+        conn = None
         try:
             conn = sqlite.connect(encrypted_db)
             cursor = conn.cursor()
@@ -59,12 +83,17 @@ def decryptDB(keys, path):
             cursor.close()
             conn.close()
             count += 1
-        except:
+        except Exception as e:
             print(f"失败：{db}")
-        os.remove(encrypted_db)
+        finally:
+            if conn:
+                conn.close()
+            if not remove_file(encrypted_db):
+                print(f"最终删除文件 {encrypted_db} 失败，请手动删除")
     return count, len(keys)
 
 
+print("开始解密数据库，请稍等...")
 keys = getKeys(uid, path)
 newPath = removeHead(keys, path)
 count, total = decryptDB(keys, newPath)
